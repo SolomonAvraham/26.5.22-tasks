f94f8201b6cebb361907df2ba9e26ddd5ebc3962
SolomonAvraham <Selmon_a@hotmail.com>
2022-05-26 16:26:27 +0300

    update

---
+++ main.js
@@ -0,0 +1,14 @@
+// 1. צרו אלמנט של טקסט ב - html עם סטרינג "first dom app"
+//  והדפיסו ללוג את התוכן שלו.
+
+console.log(one.innerText);
+
+// 2. צרו אלמנט טקסט וכתבו לתוכו ״
+// my dom app״ והדפיסו ללוגו את התוכן.
+
+document.write(two.innerHtml = "my dom app");
+
+// 3. צרו 4 אלנטי טקסט מסוג שונה ב - html
+// עם טקסטים שונים  והדפיסו אותם ללוג.
+
+console.log(three);
