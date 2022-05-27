7550dc504f549d13252f218f0f817994b49ff7da
SolomonAvraham <Selmon_a@hotmail.com>
2022-05-27 23:22:59 +0300

    update

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ console.log(one.innerText);
 // 2. צרו אלמנט טקסט וכתבו לתוכו ״
 // my dom app״ והדפיסו ללוגו את התוכן.
 
-document.write((two.innerHtml = "my dom app"));
+console.log((two.innerHtml = "my dom app"));
 
 // 3.	צרו 4 אלמנטי טקסט מסוג שונה ב html
 //  עם טקסטים שונים והדפיסו אותם ללוג.
@@ -43,11 +43,49 @@ function userNameColorAndNum() {
   for (var i = 0; i < numberOfI; i++) {
     var userColor = prompt("Color:");
     var userNum = +prompt("Number:");
-    document.write("<br>"+"<span>" + " " + userColor + " " + "</span>");
+    document.write("<br>" + "<span>" + " " + userColor + " " + "</span>");
   }
 }
 // userNameColorAndNum();
 
 // 9.	צרו פונקציה המקבלת מהמשתמש טקסט ושם תגית ואז בונה את התגית עם הטקסט ב DOM.
 
+function creatTagAndText() {
+  var userText = prompt("Text:");
+  var userTag1 = prompt("<tag>:");
+  var userTag2 = prompt("</tag>:");
+  document.getElementById("five").innerHTML =
+    userTag1 + " " + userText + " " + userTag2;
+}
+// creatTagAndText();
 
+// 10.	צרו פונקציה המקבלת מהמשתמש סוג קלט ויוצרת את הקלט המתאים ב DOM.
+
+function inPutFromUser() {
+  var userType = prompt("Enter Input type:", "color,text,date etc'");
+  var body = document.body;
+  var input = document.createElement("input");
+  input.type = userType;
+  body.append(input);
+}
+
+// inPutFromUser();
+
+// 11.	צרו פונקציה המקבלת כארגומנט שם
+// class וטקסט, הפונקציה משנה את התוכן של כל האלמנטים עם ה class שהתקבל לטקסט שהמשתמש הכניס.
+
+function funcChangingContant(six) {}
+
+// לא פתור!
+
+// 12.	צרו 6 אלמנטי P ריקים ב html, צרו פונקציה שמכניסה להם אלמנט SPAN עם טקסט.
+
+function creatContantToHtml() {
+  for (var i = 0; i < 6; i++) {
+    var paragraph = document.querySelector(".seven");
+    paragraph.innerHTML = "<span>HELLO WORLD</span>";
+  }
+
+  console.log(paragraph);
+}
+creatContantToHtml();
